9ea803f159b30d5b884c7f526d6696558f596f31
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-05-21 16:32:23 +0100

    work

--- script.py
+++ script.py
@@ -1,52 +1,16 @@
 from solution import Solution, TreeNode
 
 solution = Solution()
-
-def build_tree(values):
-    if not values:
-        return None
-
-    nodes = [TreeNode(x) if x is not None else None for x in values]
-    kids = nodes[::-1]
-    root = kids.pop()
-
-    for node in nodes:
-        if node:
-            if kids:
-                node.left = kids.pop()
-            if kids:
-                node.right = kids.pop()
-    return root
-
 # Main test
+
+
 if __name__ == '__main__':
 
     try:
-        tree_list = [1, 4, 3, 7, 6, 8, 5, None, None, None, None, 9, None, 10]
-        root = build_tree(tree_list)
         solution = Solution()
-        r = solution.run(root)
+        r = solution.run()
         assert r == 3
         print("Test passed")
     except AssertionError:
         print("Test failed")
 
-    try:
-        tree_list = [1,3,2,7,6,5,4]
-        root = build_tree(tree_list)
-        solution = Solution()
-        r = solution.run(root)
-        assert r == 3
-        print("Test passed")
-    except AssertionError:
-        print("Test failed")
-
-    try:
-        tree_list = [1,2,3,4,5,6]
-        root = build_tree(tree_list)
-        solution = Solution()
-        r = solution.run(root)
-        assert r == 0
-        print("Test passed")
-    except AssertionError:
-        print("Test failed")
--- solution.py
+++ solution.py
@@ -10,49 +10,5 @@ class TreeNode:
 
 class Solution:
 
-    def min_swaps_to_sort(self, arr: List[int]) -> int:
-        sorted_arr = sorted(arr)
-        # [1, 2, 3, 4, 5, 6, 7]
-        hash_ = {value: id for id, value in enumerate(arr)}
-        # {1: 0, 3: 1, 2: 2, 7: 3, 6: 4, 5: 5, 4: 6}
-        # hash_ uses to identify the place where the element in sorted arr is in the un-sorted array
-        # so that, can be swap in the old array.
-        swaps = 0
-
-        for i in range(len(arr)):
-            if arr[i] != sorted_arr[i]:
-                swaps += 1
-
-                # tracking arr[i]
-                j = hash_[sorted_arr[i]]
-                arr[i], arr[j] = arr[j], arr[i]
-
-                #update the hash_, so that at then end the hash_ should have the same value to index as a sorted array.
-                hash_[arr[j]] = j
-                hash_[arr[i]] = i
-        return swaps
-
     def run(self, root: Optional[TreeNode]) -> int:
-
-        if not root:
-            return 0
-
-        queue = deque([root])
-        total_swaps = 0
-
-        while queue:
-            level_size = len(queue)
-            level_values = []
-
-            for _ in range(level_size):
-                node = queue.popleft()
-                level_values.append(node.val)
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            total_swaps += self.min_swaps_to_sort(level_values)
-
-        return total_swaps
+        return 1
