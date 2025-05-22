b599f01c0afe2c99f420e62d051f18b369ad50a5
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-05-22 13:23:16 +0100

    work

--- script.py
+++ script.py
@@ -1,18 +1,37 @@
+from collections import deque
+
 from solution import Solution, TreeNode
 
 solution = Solution()
+
+
 # Main test
+def create_tree(arr):
+    if not arr:
+        return None
+
+    nodes = [TreeNode(var) for var in arr]
+    leaves = deque(nodes[::-1])
+    root = leaves.pop()
+
+    for node in nodes:
+        if node:
+            if leaves:
+                node.left = leaves.pop()
+            if leaves:
+                node.right = leaves.pop()
+    return root
 
 
 if __name__ == '__main__':
-    test_1 = [0, -1]
-    test_2 = [1, 2, 3]
-    test_3 = [1, 3, 2, 5, 3, None, 9]
+    test_1 = [3, 9, 20, None, None, 15, 7]
+    test_2 = [1]
+    test_3 = []
     tests = [test_1, test_2, test_3]
     answers = [
-        [0, -1],
-        [1, 3],
-        [1, 3, 9]
+        [[3], [20, 9], [15, 7]],
+        [[1]],
+        []
     ]
     for indx, test in enumerate(tests):
         rooter = create_tree(test)
