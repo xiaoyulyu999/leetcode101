730a346919411c0f7bb845db075014c9db4ca635
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-05-22 15:52:49 +0100

    work

--- script.py
+++ script.py
@@ -1,18 +1,37 @@
+from collections import deque
+
 from solution import Solution, TreeNode
 
 solution = Solution()
+
+
 # Main test
+
+def create_tree(arr):
+    if not arr:
+        return None
+
+    nodes = [TreeNode(var) for var in arr]
+    kids = deque(nodes[::-1])
+    root = kids.pop()
+
+    for node in nodes:
+        if kids:
+            node.left = kids.pop()
+        if kids:
+            node.right = kids.pop()
+
+    return root
 
 
 if __name__ == '__main__':
-    test_1 = [0, -1]
-    test_2 = [1, 2, 3]
+    test_1 = [3, 9, 20, None, None, 15, 7]
+    test_2 = [2, None, 3, None, 4, None, 5, None, 6]
     test_3 = [1, 3, 2, 5, 3, None, 9]
-    tests = [test_1, test_2, test_3]
+    tests = [test_1, test_2]
     answers = [
-        [0, -1],
-        [1, 3],
-        [1, 3, 9]
+        2,
+        5
     ]
     for indx, test in enumerate(tests):
         rooter = create_tree(test)
--- solution.py
+++ solution.py
@@ -1,14 +1,30 @@
-from typing import List, Optional
 from collections import deque
+from typing import Optional
 
-# Definition for a binary tree node.
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-class Solution:
 
+class Solution:
     def run(self, root: Optional[TreeNode]) -> int:
-        return 1
+        if not root:
+            return 0
+
+        q = deque([(root, 0)])  # (node, depth)
+
+        while q:
+            for _ in range(len(q)):
+                node, depth = q.popleft()
+                # Check if it's a leaf node
+                if not node.left and not node.right:
+                    print(node.val, depth)
+                    return depth + 1
+                if node.left:
+                    q.append((node.left, depth + 1))
+                if node.right:
+                    q.append((node.right, depth + 1))
+        return 0
