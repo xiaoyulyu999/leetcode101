ec169b927370cd15c307095aa94514bc2fb457d7
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-05-22 10:10:34 +0100

    work

--- script.py
+++ script.py
@@ -1,16 +1,41 @@
+from collections import deque
+
 from solution import Solution, TreeNode
 
 solution = Solution()
+
+
 # Main test
+
+def create_tree(arr):
+    nodes = [TreeNode(var) for var in arr]
+    queue = deque(nodes[::-1])
+
+    root = queue.pop()
+    for node in nodes:
+        if node:
+            node.left = queue.pop() if queue else None
+            node.right = queue.pop() if queue else None
+    return root
 
 
 if __name__ == '__main__':
-
-    try:
-        solution = Solution()
-        r = solution.run()
-        assert r == 3
-        print("Test passed")
-    except AssertionError:
-        print("Test failed")
-
+    test_1 = ([1, 2, 3], [1, 2, 3])
+    test_2 = ([1, 2], [1, None, 2])
+    test_3 = ([1, 2, 1], [1, 1, 2])
+    tests = [test_1, test_2, test_3]
+    answers = [
+        True, False, False
+    ]
+    for indx, test in enumerate(tests):
+        l, r = test
+        l_root = create_tree(l)
+        r_root = create_tree(r)
+        try:
+            solution = Solution()
+            r = solution.run(l, r)
+            print(r)
+            assert r == answers[indx]
+            print("Test passed")
+        except AssertionError:
+            print("Test failed")
--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
-from typing import List, Optional
 from collections import deque
+from typing import Optional, Any
+
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -8,7 +9,30 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
 
-    def run(self, root: Optional[TreeNode]) -> int:
-        return 1
+    def dfs(self, root: Optional[TreeNode]) -> list[Any]:
+        q = deque([root])
+        values = []
+
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                print(f"Processing node: {node} (type {type(node)})")  # debug line
+
+                if node:
+                    values.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+                else:
+                    values.append(None)
+
+        return values
+
+    def run(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        r_p = self.dfs(p)
+        r_q = self.dfs(q)
+
+        return r_p == r_q
