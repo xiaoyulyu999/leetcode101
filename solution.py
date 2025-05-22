17b52a9aa7eb83800c18ad01e3046b6ec70783f0
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-05-22 14:07:28 +0100

    work

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
-from typing import List, Optional
 from collections import deque
+from typing import List, Optional
+
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -8,7 +9,25 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
 
-    def run(self, root: Optional[TreeNode]) -> int:
-        return 1
+    def run(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        q = deque([root])
+        res = deque()
+
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.val is not None:
+                    level.append(node.val)
+                q.append(node.left) if node.left else None
+                q.append(node.right) if node.right else None
+            if level:
+                res.appendleft(level)
+
+        return list(res)
