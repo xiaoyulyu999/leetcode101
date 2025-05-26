a949a5952de1cee0a42e3bce021b1e5128a82eae
xiaoyulyu999 <info@bloomora.net>
2025-05-26 08:17:59 +0100

    home

--- script.py
+++ script.py
@@ -1,23 +1,24 @@
-from solution import Solution, TreeNode
+from solution import Solution
 
 solution = Solution()
 # Main test
 
 
 if __name__ == '__main__':
-    test_1 = [2,7,11,15]
-    test_2 = [3,2,4]
-    test_3 = [3,3]
-    tests = [test_1, test_2, test_3]
+    test_1 = [2, 7, 11, 15]
+    test_2 = [3, 2, 4]
+    test_3 = [3, 3]
+    tests = [(test_1, 9), (test_2, 6), (test_3, 6)]
     answers = [
-        9,
-        6,
-        6
+        [0, 1],
+        [1, 2],
+        [0, 1]
     ]
     for indx, test in enumerate(tests):
         try:
+            ls, target = test
             solution = Solution()
-            r = solution.run(rooter)
+            r = solution.run(ls, target)
             print(r)
             assert r == answers[indx]
             print("Test passed")
--- solution.py
+++ solution.py
@@ -4,4 +4,12 @@ from typing import List
 class Solution:
 
     def run(self, nums: List[int], target: int) -> List[int]:
+
+        hashtable = {}
+        for idx, num in enumerate(nums):
+
+            if target - num in hashtable:
+                return [hashtable[target - num], idx]
+            else:
+                hashtable[num] = idx
         return []
