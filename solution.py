dc69795d8a929b393b463d23c7c93946ac25b619
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-05-30 16:28:03 +0100

    work

--- solution.py
+++ solution.py
@@ -2,13 +2,12 @@ class Solution:
 
     def run(self, s: str) -> int:
         charSet = set()
+        counter = 0
         l = 0
-        count = 0
         for r in range(len(s)):
             while s[r] in charSet:
                 charSet.remove(s[l])
                 l += 1
-
             charSet.add(s[r])
-            count = max(count, r - l + 1)
-        return count
+            counter = max(counter, r - l + 1)
+        return counter
